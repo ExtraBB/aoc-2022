2a12ea5ba72bf83b5bb060cc772b8b86b11ab957
Bruno Carvalhal <bruno.carvalhal@datasnipper.com>
2022-12-07 12:36:07 +0100

    wip

---
+++ aoc-2022/DataStructures/TreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2022.DataStructures
+{
+    public class TreeNode
+    {
+        public string Name { get; }
+        public Dictionary<string, TreeNode> ChildNodes { get; } = new Dictionary<string, TreeNode>();
+        public TreeNode? Parent { get; }
+
+        public TreeNode(string name, TreeNode? parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public void CreateChild(string name)
+        {
+            ChildNodes.Add(name, new TreeNode(name, this));
+        }
+
+        public TreeNode GetOrCreateChild(string name)
+        {
+            if (!ChildNodes.ContainsKey(name))
+            {
+                CreateChild(name);
+            }
+            return ChildNodes[name];
+        }
+    }
+}
---
+++ aoc-2022/Day7.cs
@@ -0,0 +1,57 @@
+﻿using aoc_2022.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2022
+{
+    public class Day7 : IDay
+    {
+        public string Part1(string filePath)
+        {
+            TreeNode rootNode = new("/", null);
+            TreeNode currentNode = rootNode;
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                if (line == "$ cd ..")
+                {
+                    currentNode = currentNode.Parent!;
+                }
+                else if (line == "$ cd /")
+                {
+                    currentNode = rootNode;
+                }
+                else if (line.StartsWith("$ cd "))
+                {
+                    string folder = line.Substring("$ cd ".Length);
+                    currentNode = currentNode.GetOrCreateChild(folder);
+                }
+                else if (line.StartsWith("$"))
+                {
+                    // ignore
+                }
+                else if (line.StartsWith("dir "))
+                {
+                    string folder = line.Substring("dir ".Length);
+
+                    currentNode.CreateChild(folder);
+                }
+            }
+        }
+
+        public string Part2(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ProcessInstruction(string instruction)
+        {
+
+        }
+    }
+
+    public class Tree 
+}
