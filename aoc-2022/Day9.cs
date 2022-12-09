57c7d9bf75032b17a96f42c1859e0d922af7c065
Bruno Carvalhal <private>
2022-12-09 15:11:31 +0100

    First solve for day 9 part 2

--- aoc-2022/Day9.cs
+++ aoc-2022/Day9.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Diagnostics.Tracing.Parsers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -14,57 +15,121 @@ namespace aoc_2022
     {
         public string Part1(string filePath)
         {
-            (Location head, Location tail) locations = (new Location(0, 0), new Location(0, 0));
+            return Run(filePath, 2);
+        }
 
-            HashSet<Location> seen = new HashSet<Location>() { new Location(locations.tail.X, locations.tail.Y) };
 
-            foreach(string instruction in File.ReadLines(filePath))
+        public string Part2(string filePath)
+        {
+            return Run(filePath, 10);
+        }
+
+        private string Run(string filePath, int ropeLength)
+        {
+            int[] xs = Enumerable.Repeat(0, ropeLength).ToArray();
+            int[] ys = Enumerable.Repeat(0, ropeLength).ToArray();
+
+            HashSet<Location> seen = new HashSet<Location>() { new Location(0, 0) };
+
+            foreach (string instruction in File.ReadLines(filePath))
             {
                 var split = instruction.Split(' ');
                 int steps = int.Parse(split[1]);
-                switch (split[0]) 
+                switch (split[0])
                 {
-                    case "U": locations = MoveSteps(locations.head, locations.tail, 0, -1, steps, seen); break;
-                    case "D": locations = MoveSteps(locations.head, locations.tail, 0, 1, steps, seen); break;
-                    case "L": locations = MoveSteps(locations.head, locations.tail, -1, 0, steps, seen); break;
-                    case "R": locations = MoveSteps(locations.head, locations.tail, 1, 0, steps, seen); break;
+                    case "U": MoveSteps(xs, ys, 0, -1, steps, seen); break;
+                    case "D": MoveSteps(xs, ys, 0, 1, steps, seen); break;
+                    case "L": MoveSteps(xs, ys, -1, 0, steps, seen); break;
+                    case "R": MoveSteps(xs, ys, 1, 0, steps, seen); break;
                 }
             }
 
             return seen.Count.ToString();
         }
 
-        private (Location head, Location tail) MoveSteps(Location head, Location tail, int dx, int dy, int steps, HashSet<Location> seen)
+        private void PrintGrid(int[] xs, int[] ys, int size)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = -size/2; y < size / 2; y++)
+            {
+                string line = "";
+                for (int x = -size / 2; x < size / 2; x++)
+                {
+                    bool hit = false;
+                    for (int i = 0; i < xs.Length; i++)
+                    {
+                        if (xs[i] == x && ys[i] == y)
+                        {
+                            line += i == 0 ? "H" : i.ToString();
+                            hit = true; break;
+                        }
+                    }
+
+                    if (!hit)
+                    {
+                        line += ".";
+                    }
+                }
+                sb.AppendLine(line);
+            }
+
+            sb.AppendLine();
+            Debug.Write(sb.ToString());
+        }
+
+        private void MoveSteps(int[] xs, int[] ys, int dx, int dy, int steps, HashSet<Location> seen)
         {
             for (int i = 0; i < steps; i++)
             {
-                Location prevHead = head;
-                head = new Location(head.X + dx, head.Y + dy);
+                int[] prevXs = xs.ToArray();
+                int[] prevYs = ys.ToArray();
 
-                int xDist = Math.Abs(head.X - tail.X);
-                int yDist = Math.Abs(head.Y - tail.Y);
-                int totalDist = xDist + yDist;
-                if (xDist > 0 & yDist > 0)
+                xs[0] += dx;
+                ys[0] += dy;
+
+                for(int j = 1; j < xs.Length; j++)
                 {
-                    if (totalDist > 2)
+                    int xDist = Math.Abs(xs[j - 1] - xs[j]);
+                    int yDist = Math.Abs(ys[j - 1] - ys[j]);
+
+                    int xDistPrev = Math.Abs(prevXs[j - 1] - prevXs[j]);
+                    int yDistPrev = Math.Abs(prevYs[j - 1] - prevYs[j]);
+
+                    bool gotPulled = xDist > 1 || yDist > 1;
+
+                    if (gotPulled)
                     {
-                        tail = prevHead;
+                        if (xDistPrev + yDistPrev == 1)
+                        {
+                            xs[j] = xs[j - 1] - (prevXs[j - 1] - prevXs[j]);
+                            ys[j] = ys[j - 1] - (prevYs[j - 1] - prevYs[j]);
+                        }
+                        else if (xDist + yDist > 2)
+                        {
+                            xs[j] = prevXs[j - 1];
+                            ys[j] = prevYs[j - 1];
+                        }
+                        else if (yDist == 2 && xDist == 0)
+                        {
+                            int distY = ys[j - 1] - ys[j];
+                            ys[j] = ys[j] + (distY > 0 ? distY - 1 : distY + 1);
+                        }
+                        else if (xDist == 2 && yDist == 0)
+                        {
+                            int distX = xs[j - 1] - xs[j];
+                            xs[j] = xs[j] + (distX > 0 ? distX - 1 : distX + 1);
+                        }
+                    }
+
+                    if (j == xs.Length - 1)
+                    {
+                        seen.Add(new Location(xs[j], ys[j]));
                     }
                 }
-                else if (totalDist > 1)
-                {
-                    tail = prevHead;
-                }
-
-                seen.Add(tail);
+                PrintGrid(xs, ys, 26);
             }
-
-            return (head, tail);
-        } 
-
-        public string Part2(string filePath)
-        {
-            throw new NotImplementedException();
         }
+
     }
 }
