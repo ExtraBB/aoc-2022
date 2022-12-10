b5a702f6f0b5eb77ff9498fcfd7dbf42f2be8301
Bruno Carvalhal <private>
2022-12-10 13:31:39 +0100

    Finish day 10

--- aoc-2022/Day10.cs
+++ aoc-2022/Day10.cs
@@ -1,4 +1,6 @@
-﻿namespace aoc_2022
+﻿using System.Text;
+
+namespace aoc_2022
 {
     internal class Day10 : IDay
     {
@@ -37,7 +39,48 @@
 
         public string Part2(string filePath)
         {
-            throw new NotImplementedException();
+            int x = 1;
+            int cycle = 1;
+
+            StringBuilder sb = new StringBuilder();
+            string pixels = "";
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                pixels += SpriteVisible(x, cycle) ? "#" : ".";
+                if (cycle % 40 == 0)
+                {
+                    sb.AppendLine(pixels);
+                    pixels = "";
+                }
+
+                if (line != "noop")
+                {
+                    int toAdd = int.Parse(line.Substring("addx ".Length));
+
+                    cycle++;
+
+                    pixels += SpriteVisible(x, cycle) ? "#" : ".";
+                    if (cycle % 40 == 0)
+                    {
+                        sb.AppendLine(pixels);
+                        pixels = "";
+                    }
+
+                    x += toAdd;
+                }
+
+                cycle++;
+            }
+
+            return sb.ToString();
+        }
+
+        private bool SpriteVisible(int x, int cycle)
+        {
+            return new int[] { ((cycle - 1) % 40) - 1, (cycle - 1) % 40, ((cycle - 1) % 40) + 1 }
+                .Where(c => c >= 0 && c < 40)
+                .Any(c => c == x);
         }
     }
 }
