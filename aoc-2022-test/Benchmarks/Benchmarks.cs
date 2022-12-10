02b22dec68f588872410d46a710c83a7bf253759
Bruno Carvalhal <private>
2022-12-10 13:41:57 +0100

    Polish day 10

--- aoc-2022-test/Benchmarks/Benchmarks.cs
+++ aoc-2022-test/Benchmarks/Benchmarks.cs
@@ -4,100 +4,124 @@ namespace aoc_2022_test.Benchmarks
 {
     public class DaysBenchmark
     {
-        [BenchmarkDotNet.Attributes.Arguments("./Files/1-actual.txt")]
+        [BenchmarkDotNet.Attributes.Arguments("./Inputs/Actual/1.txt")]
         public void Day1Part1()
         {
-            new Day1().Part1("./Files/1-actual.txt");
+            new Day1().Part1("./Inputs/Actual/1.txt");
         }
 
         [Benchmark]
         public void Day1Part2()
         {
-            new Day1().Part2("./Files/1-actual.txt");
+            new Day1().Part2("./Inputs/Actual/1.txt");
         }
 
         [Benchmark]
         public void Day2Part1()
         {
-            new Day2().Part1("./Files/2-actual.txt");
+            new Day2().Part1("./Inputs/Actual/2.txt");
         }
 
         [Benchmark]
         public void Day2Part2()
         {
-            new Day2().Part2("./Files/2-actual.txt");
+            new Day2().Part2("./Inputs/Actual/2.txt");
         }
 
         [Benchmark]
         public void Day3Part1()
         {
-            new Day3().Part1("./Files/3-actual.txt");
+            new Day3().Part1("./Inputs/Actual/3.txt");
         }
 
         [Benchmark]
         public void Day3Part2()
         {
-            new Day3().Part2("./Files/3-actual.txt");
+            new Day3().Part2("./Inputs/Actual/3.txt");
         }
 
         [Benchmark]
         public void Day4Part1()
         {
-            new Day4().Part1("./Files/4-actual.txt");
+            new Day4().Part1("./Inputs/Actual/4.txt");
         }
 
         [Benchmark]
         public void Day4Part2()
         {
-            new Day4().Part2("./Files/4-actual.txt");
+            new Day4().Part2("./Inputs/Actual/4.txt");
         }
 
         [Benchmark]
         public void Day5Part1()
         {
-            new Day5().Part1("./Files/5-actual.txt");
+            new Day5().Part1("./Inputs/Actual/5.txt");
         }
 
         [Benchmark]
         public void Day5Part2()
         {
-            new Day5().Part2("./Files/5-actual.txt");
+            new Day5().Part2("./Inputs/Actual/5.txt");
         }
 
         [Benchmark]
         public void Day6Part1()
         {
-            new Day6().Part1("./Files/6-actual.txt");
+            new Day6().Part1("./Inputs/Actual/6.txt");
         }
 
         [Benchmark]
         public void Day6Part2()
         {
-            new Day6().Part2("./Files/6-actual.txt");
+            new Day6().Part2("./Inputs/Actual/6.txt");
         }
 
         [Benchmark]
         public void Day7Part1()
         {
-            new Day7().Part1("./Files/7-actual.txt");
+            new Day7().Part1("./Inputs/Actual/7.txt");
         }
 
         [Benchmark]
         public void Day7Part2()
         {
-            new Day7().Part2("./Files/7-actual.txt");
+            new Day7().Part2("./Inputs/Actual/7.txt");
         }
 
         [Benchmark]
         public void Day8Part1()
         {
-            new Day8().Part1("./Files/8-actual.txt");
+            new Day8().Part1("./Inputs/Actual/8.txt");
         }
 
         [Benchmark]
         public void Day8Part2()
         {
-            new Day8().Part2("./Files/8-actual.txt");
+            new Day8().Part2("./Inputs/Actual/8.txt");
+        }
+
+        [Benchmark]
+        public void Day9Part1()
+        {
+            new Day9().Part1("./Inputs/Actual/9.txt");
+        }
+
+        [Benchmark]
+        public void Day9Part2()
+        {
+            new Day9().Part2("./Inputs/Actual/9.txt");
+        }
+
+        [Benchmark]
+        public void Day10Part1()
+        {
+            new Day10().Part1("./Inputs/Actual/10.txt");
+        }
+
+        [Benchmark]
+        public void Day10Part2()
+        {
+            new Day10().Part2("./Inputs/Actual/10.txt");
         }
     }
 }
--- aoc-2022/Day10.cs
+++ aoc-2022/Day10.cs
@@ -2,85 +2,61 @@
 
 namespace aoc_2022
 {
-    internal class Day10 : IDay
+    public class Day10 : IDay
     {
         public string Part1(string filePath)
         {
-            var cyclesToCount = new HashSet<int>() { 20, 60, 100, 140, 180, 220 };
-            int x = 1;
-            int cycle = 1;
             int result = 0;
 
-            foreach (string line in File.ReadLines(filePath))
+            Run(filePath, (int x, int cycle) =>
             {
-                if (cyclesToCount.Contains(cycle))
+                if ((cycle + 20) % 40 == 0)
                 {
-                    result += cycle * x;
+                    result += x * cycle;
                 }
-
-                if (line != "noop")
-                {
-                    int toAdd = int.Parse(line.Substring("addx ".Length));
-
-                    cycle++;
-                    if (cyclesToCount.Contains(cycle))
-                    {
-                        result += cycle * x;
-                    }
-
-                    x += toAdd;
-                }
-
-                cycle++;
-            }
+            });
 
             return result.ToString();
         }
 
         public string Part2(string filePath)
         {
-            int x = 1;
-            int cycle = 1;
-
             StringBuilder sb = new StringBuilder();
-            string pixels = "";
 
-            foreach (string line in File.ReadLines(filePath))
+            Run(filePath, (int x, int cycle) =>
             {
-                pixels += SpriteVisible(x, cycle) ? "#" : ".";
+                bool spriteVisible = new int[] { ((cycle - 1) % 40) - 1, (cycle - 1) % 40, ((cycle - 1) % 40) + 1 }
+                    .Where(c => c >= 0 && c < 40)
+                    .Any(c => c == x);
+
+                sb.Append(spriteVisible ? "#" : ".");
                 if (cycle % 40 == 0)
                 {
-                    sb.AppendLine(pixels);
-                    pixels = "";
+                    sb.Append("\r\n");
                 }
-
-                if (line != "noop")
-                {
-                    int toAdd = int.Parse(line.Substring("addx ".Length));
-
-                    cycle++;
-
-                    pixels += SpriteVisible(x, cycle) ? "#" : ".";
-                    if (cycle % 40 == 0)
-                    {
-                        sb.AppendLine(pixels);
-                        pixels = "";
-                    }
-
-                    x += toAdd;
-                }
-
-                cycle++;
-            }
+            });
 
             return sb.ToString();
         }
 
-        private bool SpriteVisible(int x, int cycle)
+        private void Run(string filePath, Action<int, int> action)
         {
-            return new int[] { ((cycle - 1) % 40) - 1, (cycle - 1) % 40, ((cycle - 1) % 40) + 1 }
-                .Where(c => c >= 0 && c < 40)
-                .Any(c => c == x);
+            int x = 1;
+            int cycle = 1;
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                action(x, cycle);
+
+                if (line != "noop")
+                {
+                    cycle++;
+                    action(x, cycle);
+                    x += int.Parse(line.Substring("addx ".Length));
+                }
+
+                cycle++;
+            }
         }
     }
 }
