2176200c6c0b9cb77e43b82540b2161f613d7981
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-08-28 17:09:12 +0200

    Add runner program

---
+++ aoc-2022-runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using aoc_2022.Days;
+using System.Linq;
+using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int day = int.Parse(args[0]);
+        int part = int.Parse(args[1]);
+        string folder = args[2];
+
+        Type t = Type.GetType($"aoc_2022.Days.Day{day},aoc-2022");
+        IDay dayObj = (IDay)Activator.CreateInstance(t);
+
+        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $"./{folder}/{day}.txt");
+        Console.WriteLine("Result: " + (part == 1 ? dayObj.Part1(path) : dayObj.Part2(path)));
+    }
+}
