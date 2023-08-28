248c1bef74cdbed64d80d93448054a9168d5a5cd
Bruno Carvalhal <hello@brunocarvalhal.com>
2023-08-28 17:10:03 +0200

    Add day14 scaffolding

---
+++ aoc-2022/Days/Day14.cs
@@ -0,0 +1,14 @@
+namespace aoc_2022.Days;
+
+public class Day14 : IDay
+{
+    public string Part1(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+}
